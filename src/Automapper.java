096e931318ef0325c6dab2e10d059e5abcb5f9e9
razoring <blotzytgaming@gmail.com>
2024-12-29 14:20:39 -0500

    super class, needs to fix rendering

--- src/Automapper.java
+++ src/Automapper.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 import javax.swing.*;
 
-public class Automapper extends JPanel {
+public class Automapper extends GPSBase {
 	Image mapImage;
 	static String mode = "ADD";
 	LinkedList<Node> nodes = new LinkedList<>();
@@ -19,6 +19,7 @@ public class Automapper extends JPanel {
 	static Automapper panel = new Automapper("src/8.PNG");
 
 	public Automapper(String imagePath) {
+		super(imagePath);
 		mapImage = new ImageIcon(imagePath).getImage();
 		
 		readFromFile();
@@ -35,7 +36,7 @@ public class Automapper extends JPanel {
 		});
 	}
 
-	private void drawMap(int x, int y, Node node) {
+	void drawMap(int x, int y, Node node) {
 		if (mode.equals("ADD")) {
 			if (findNearestNode(x,y,10)==null) {
 				Node newNode = new Node(x, y, "INTERSECTION");
@@ -93,6 +94,7 @@ public class Automapper extends JPanel {
         }
 		
 		saveToFile();
+		frame.repaint();
 	}
 
 	File save = new File("src//map.txt");
@@ -115,125 +117,9 @@ public class Automapper extends JPanel {
 			e.printStackTrace();
 		}
 	}
-	
-	private void readFromFile() {
-		try {
-			try {
-				if (save.createNewFile()) {
-					System.out.println("File created: " + save.getName());
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			// generate all the nodes first
-			Scanner reader = new Scanner(save);
-			while (reader.hasNextLine()) {
-				String data = reader.nextLine();
-				String unpacked = data.substring(1,data.length()-1);
-				String items[] = unpacked.split("\n");
-				for (String item : items) {
-					String curr = item.split("\\),")[0];
-					String next = item.split("\\),")[1];
-					String prev = item.split("\\),")[2];
-
-					String xx = curr.split(",")[0].replaceAll("\\D", "");
-					if (!xx.isEmpty()) {
-						int x = Integer.parseInt(xx);
-						String yy = curr.split(",")[1].replaceAll("\\)", "");
-						if (!yy.isEmpty()) {
-							int y = Integer.parseInt(yy);
-							String type = curr.split("\\.")[1].split("\\(")[0];
-
-							//System.out.println("o-"+x+","+y);
-							Node newNode = new Node(x, y, type);
-									
-							nodes.add(newNode);
-							if (type.charAt(0) == 'I') {
-								intersections.add(newNode);
-							} else {
-								curves.add(newNode);
-							}
-						}
-					}
-				}
-			}
-			
-			// attach all the previous and next nodes, will not connect if a node is not found
-			Scanner reader2 = new Scanner(save);
-			while (reader2.hasNextLine()) {
-				String data = reader2.nextLine();
-				String unpacked = data.substring(1,data.length()-1);
-				String items[] = unpacked.split("\n");
-				
-				for (String item : items) {
-					String curr = item.split("\\),")[0];
-					String next = item.split("\\),")[1];
-					String prev = item.split("\\),")[2];
-					
-					String xx = curr.split(",")[0].replaceAll("\\D", "");
-					if (!xx.isEmpty()) {
-						int x = Integer.parseInt(xx);
-						String yy = curr.split(",")[1].replaceAll("\\)", "");
-						if (!yy.isEmpty()) {
-							int y = Integer.parseInt(yy);
-							
-							Node newNode = findNearestNode(x,y,10);
-							
-							//System.out.println("o-"+x+","+y);
-							xx = next.split(",")[0].replaceAll("\\D", "");
-							if (!xx.isEmpty()) {
-								int x_ = Integer.parseInt(xx);
-								yy = next.split(",")[1].replaceAll("\\)", "");
-								if (!yy.isEmpty()) {
-									int y_ = Integer.parseInt(yy);
-									newNode.next = findNearestNode(x_,y_,10);
-									//System.out.println("n-"+x_+","+y_);
-								}
-							}
-							
-							xx = prev.split(",")[0].replaceAll("\\D", "");
-							if (!xx.isEmpty()) {
-								int x_ = Integer.parseInt(xx);
-								yy = prev.split(",")[1].replaceAll("\\)", "");
-								if (!yy.isEmpty()) {
-									int y_ = Integer.parseInt(yy);
-									newNode.prev = findNearestNode(x_,y_,10);
-									//System.out.println("p-"+x_+","+y_);
-								}
-							}
-						}
-					}
-				}
-			}
-			reader.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private Node findNearestNode(int x, int y, int tolerance) {
-		for (Node node : nodes) {
-			if (Math.abs(node.x - x) <= tolerance && Math.abs(node.y - y) <= tolerance) {
-				return node;
-			}
-		}
-		return null;
-	}
-	
-	private Node findNext(Node node) {
-		if (node.next.type().equals("CURVE")) {
-			return findNext(node.next);
-		} else {
-			return node.next;
-		}
-	}
 
 	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
+	void draw(Graphics g) {
 		g.drawImage(mapImage, 0, 0, getWidth(), getHeight(), this);
 
 		for (Node node : intersections) {
@@ -269,5 +155,11 @@ public class Automapper extends JPanel {
 			selectedNode1 = null;
 			selectedNode2 = null;
 		}
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		this.draw(g);
 	}
 }
