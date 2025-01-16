7e0ba448792b19b06d7a2b6ff57bc2b1d8239ba9
JW <166519339+Scientific-Railgun@users.noreply.github.com>
2025-01-16 14:34:00 -0500

    +
    + readded try statement

--- src/GPSApp.java
+++ src/GPSApp.java
@@ -13,94 +13,97 @@ import javax.swing.*;
 public class GPSApp extends GPSBase {
 	static JFrame frame = new JFrame("Map with Mouse Listener");
 	static GPSApp panel = new GPSApp("src/8.PNG");
-
-	public GPSApp(String imagePath) {
-        super(imagePath);
-
-		print("Loading nodes... please be patient");
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int x = e.getX();
-                int y = e.getY();
-                int nodeType = InterfaceUI.nodeSelection;
-
-                //print(findNearestNode(x,y,10));
-                //print(Arrays.deepToString(nodes.toArray()));
-                //print(findConnections(findNearestNode(x,y,50)).size());
-				if (nodeType > 0) {
-					if (selectedNode1 == null || nodeType == 1) {
-						selectedNode1 = findNearestNode(x,y,10);
-						InterfaceUI.start.setText("(" + selectedNode1.x + ", " + selectedNode1.y + ")");
-					} else if ((selectedNode1 != null && selectedNode2 == null) || nodeType == 2) {
-						selectedNode2 = findNearestNode(x,y,10);
-						InterfaceUI.destination.setText("(" + selectedNode2.x + ", " + selectedNode2.y + ")");
-						//Temporarily commented out. Set selectedNode1 and selectedNode2 to null AFTER algo runs
+	
+		public GPSApp(String imagePath) {
+			super(imagePath);
+	
+			print("Loading nodes... please be patient");
+			addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					try {
+						int x = e.getX();
+					int y = e.getY();
+					int nodeType = InterfaceUI.nodeSelection;
+	
+					//print(findNearestNode(x,y,10));
+					//print(Arrays.deepToString(nodes.toArray()));
+					//print(findConnections(findNearestNode(x,y,50)).size());
+					if (nodeType > 0) {
+						if (selectedNode1 == null || nodeType == 1) {
+							selectedNode1 = findNearestNode(x,y,10);
+							InterfaceUI.start.setText("(" + selectedNode1.x + ", " + selectedNode1.y + ")");
+						} else if ((selectedNode1 != null && selectedNode2 == null) || nodeType == 2) {
+							selectedNode2 = findNearestNode(x,y,10);
+							InterfaceUI.destination.setText("(" + selectedNode2.x + ", " + selectedNode2.y + ")");
+						}
+	
+						InterfaceUI.nodeSelection = 0;
 					}
-
-					InterfaceUI.nodeSelection = 0;
+					} catch (Exception NullPointerException) {
+						System.err.println("Node not found.");
+					}
+					
 				}
-                
-            }
-        });
-    }
-
+			});
+		}
 	
-	@Override
-	void draw(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		Color lvl[] = {Color.green, new Color(226,186,52),Color.red};
-		g.drawImage(mapImage, 0, 0, getWidth(), getHeight(), this);
 		
-		for (Node node : nodes) {
-			for (Integer value : node.congestion) {
-				if (node.type.equals("CURVE")) {
-					// print(value);
+		@Override
+		void draw(Graphics g) {
+			Graphics2D g2d = (Graphics2D) g;
+			Color lvl[] = {Color.green, new Color(226,186,52),Color.red};
+			g.drawImage(mapImage, 0, 0, getWidth(), getHeight(), this);
+			
+			for (Node node : nodes) {
+				for (Integer value : node.congestion) {
+					if (node.type.equals("CURVE")) {
+						// print(value);
+					}
+					g.setColor(lvl[value]);
 				}
-				g.setColor(lvl[value]);
-			}
-
-			if (node.next != null) {
-				g2d.setStroke(new BasicStroke(5));
-				for (Node next : node.next) {
-					g.drawLine(node.x, node.y, next.x, next.y);
-				}
-			}
-		}
-
-		for (Node node : intersections) {
-			if (nodes.contains(node)) {
-				if (node.marker) {
-					g.setColor(Color.blue);
-				} else {
-					g.setColor(Color.black);
-				}
-				if (node.connections != null) {
-					Font text = new Font(Font.SANS_SERIF, Font.BOLD, 8);
-					g2d.setFont(text);
-					g2d.drawString((int)(node.getSpeed()) + "km/h", node.x, node.y - 5);
-				}
-				if (node.size == 1) {
-					g.fillOval(node.x - 5, node.y - 5, 10, 10);
-				} else {
-					g.fillOval(node.x - 3, node.y - 3, 6, 6);
-				}
-			}
-		}
-	}
 	
-	public void generateTraffic() {
-		for (Node node : nodes) {
-			if (nodes.contains(node)) {
-	    		node.marker = false;
-				node.setTraffic();
+				if (node.next != null) {
+					g2d.setStroke(new BasicStroke(5));
+					for (Node next : node.next) {
+						g.drawLine(node.x, node.y, next.x, next.y);
+					}
+				}
+			}
+	
+			for (Node node : intersections) {
+				if (nodes.contains(node)) {
+					if (node.marker) {
+						g.setColor(Color.blue);
+					} else {
+						g.setColor(Color.black);
+					}
+					if (node.connections != null) {
+						Font text = new Font(Font.SANS_SERIF, Font.BOLD, 8);
+						g2d.setFont(text);
+						g2d.drawString((int)(node.getSpeed()) + "km/h", node.x, node.y - 5);
+					}
+					if (node.size == 1) {
+						g.fillOval(node.x - 5, node.y - 5, 10, 10);
+					} else {
+						g.fillOval(node.x - 3, node.y - 3, 6, 6);
+					}
+				}
 			}
 		}
-	}
-
-	public static void main(String[] args) {
-		frame.setCursor(Cursor.CROSSHAIR_CURSOR);
-		frame.add(panel);
+		
+		public void generateTraffic() {
+			for (Node node : nodes) {
+				if (nodes.contains(node)) {
+					node.marker = false;
+					node.setTraffic();
+				}
+			}
+		}
+	
+		public static void main(String[] args) {
+			frame.setCursor(Cursor.CROSSHAIR_CURSOR);
+			frame.add(panel);
 		frame.setSize(1200, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -160,7 +163,7 @@ public class GPSApp extends GPSBase {
 	            if (!path.isEmpty()) {
 	                path.pop();
 	                if (!path.isEmpty()) {
-	                    return algorithm("Distance", start, path.getLast(), end, path, visited, modifiers);
+	                    return algorithm("Distance", start, path.lastElement(), end, path, visited, modifiers);
 	                }
 	            }
 	            return null;
