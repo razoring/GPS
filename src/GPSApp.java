d368d0c70e61e24882adea3ff887856c022b76f8
JW <166519339+Scientific-Railgun@users.noreply.github.com>
2025-01-18 23:44:23 -0500

    Update GPSApp.java

--- src/GPSApp.java
+++ src/GPSApp.java
@@ -168,7 +168,7 @@ public class GPSApp extends GPSBase {
 			if (mod.contains("traffic")) {
 		        weight *= current.getTraffic() + 2;
 			}
-			if (mode.contains("speed")) {
+			if (mod.contains("speed")) {
 				weight /= current.getSpeed();
 			}
 		}
